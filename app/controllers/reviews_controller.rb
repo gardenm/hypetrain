3a70ef3b73a761547233bcd8e91d0af1ed0fb82c
Matthew Garden <matt.garden@gmail.com>
2012-12-17 23:21:54 -0500

    Created a page that shows all of the albums that have a metacritic review, with a logo that links to the review on metacritic's site.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def show
+    @artists = Artist.all
+    @reviews = Review.all
+    
+    respond_to do |format|
+      format.html # show.html.haml
+      format.xml { render :xml => @reviews }
+    end
+  end
 end
