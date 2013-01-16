164926bcd5a69e843f120d7811fca42200edfa4a
Matthew Garden <matt.garden@gmail.com>
2013-01-15 22:25:19 -0500

    Removing stuff related to user models (not added yet)

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -57,43 +57,4 @@ ActiveRecord::Schema.define(:version => 20130108041606) do
   add_index "reviews", ["album_id"], :name => "index_reviews_on_album_id"
   add_index "reviews", ["critic_id"], :name => "index_reviews_on_critic_id"
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
-  create_table "settings", :force => true do |t|
-    t.integer  "user_id"
-    t.boolean  "rdio"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "settings", ["user_id"], :name => "index_settings_on_user_id"
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "provider"
-    t.string   "uid"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
 end
